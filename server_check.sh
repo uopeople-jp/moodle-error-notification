87172af173471fd68e7c05ca083b7dd2cc0656f1
kojimatetusya <gangelion8@gmail.com>
2021-05-01 16:59:14 +0900

    add schedule.yml

--- server_check.sh
+++ server_check.sh
@@ -10,4 +10,7 @@ STATUS_CODE=`grep HTTP check | awk '{print $2}'`
 
 if [ $STATUS_CODE -ne 200 ];then
     curl -X POST -H 'Content-type: application/json' --data "{'text': 'Moodle落ちてます:cry:\nステータスコード: ${STATUS_CODE}'}" $SLACK_WEBHOOK_URL
+else
+    # テストのために一旦出力
+    curl -X POST -H 'Content-type: application/json' --data "{'text': '正常です。\nステータスコード: ${STATUS_CODE}'}" $SLACK_WEBHOOK_URL
 fi
